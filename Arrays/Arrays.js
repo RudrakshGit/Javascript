0358cf57be88c098fbe838ee6890ea297fb3bdd1
RudrakshGit <rudrasoftinator@gmail.com>
2024-07-03 15:40:32 +0530

    change in arrays

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -7,11 +7,11 @@ const berries = ['strawberry', 'raspberry', 'blueberry'];
 
 //x = `${fruits[0]} ${fruits[3]}`;
 x = [...berries,...fruits];
-x = x.toString();
+//x = x.toString();
+x = x.includes('strawberry');
+//x = fruits.concat(berries);
 
-x = fruits.concat(berries);
+//const arr = [1,2,[3,4],5,6];
 
-const arr = [1,2,[3,4],5,6];
-
-x = arr.flat();
+//x = arr.flat();
 console.log(x);
