9e2383cbc5434e0ef4ef692db044228c21a3d76c
RudrakshGit <rudrasoftinator@gmail.com>
2024-07-07 17:21:21 +0530

    for...of method examples

---
+++ for..of_method/example.js
@@ -0,0 +1,22 @@
+//Loop through arrays
+const items = [
+    'book', 'table', 'chair'
+];
+
+for (const item of items){
+    console.log(item);
+}
+
+//Loop through objects
+const users = [{name:'Mukesh'},{name:'Suresh'},{name:'Ramesh'}];
+
+for (const user of users){
+    console.log(user.name);
+}
+
+//Loop through strings
+const str = "Hello World!";
+
+for (const letter of str){
+    console.log(letter);
+}
