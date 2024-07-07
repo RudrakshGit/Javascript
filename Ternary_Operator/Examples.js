33a1cd607d457c9c86d306a333b980be1dfc6ad9
RudrakshGit <rudrasoftinator@gmail.com>
2024-07-07 17:03:45 +0530

    Ternary operator examples

---
+++ Ternary_Operator/Examples.js
@@ -0,0 +1,21 @@
+const age = 13;
+
+//Using if statement
+
+if(age >= 18){
+    console.log('You can vote');
+}
+else{
+    console.log('You cannot vote');
+};
+
+//Using ternary operator "variablename = (condition) ? value1:value2"
+
+age >= 18 ? console.log('You can vote') : console.log('You cannot vote');
+
+//Assigning a conditional value to a variable
+const canVote = age >= 18 ? true : false;
+console.log(canVote);
+
+//Multiple operations
+age >= 18 ? console.log('You can vote', true) : console.log('You cannot vote',false , 'invalid age');
