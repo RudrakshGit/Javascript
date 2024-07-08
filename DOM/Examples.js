82bb94eb1b8c81b9e9770a9c88dfe9b96fe8ed65
RudrakshGit <rudrasoftinator@gmail.com>
2024-07-08 15:26:26 +0530

    DOM properties examples

---
+++ DOM/Examples.js
@@ -0,0 +1,31 @@
+console.log('Welcome!!!');
+//Finding HTML Element by Id
+
+/* const element = document.getElementById("container");
+console.log(element); */
+
+//Finding HTML Elements by Tag Name
+/* const element = document.getElementsByTagName("p");
+console.log(element); */
+
+
+/* const element = document.getElementById("container");
+newelement = element.getElementsByTagName("ul");
+onemoreelement = element.getElementsByTagName("li");
+console.log(onemoreelement);*/
+
+//Finding HTML Elements by Class Name
+
+/* const element = document.getElementsByClassName("example");
+console.log(element); */
+/*
+let x = document.querySelector("div");
+let y = document.querySelector("#content");
+
+console.log(x);
+console.log(y);
+
+*/
+
+let element = document.getElementsByTagName("div");
+console.log(element);
